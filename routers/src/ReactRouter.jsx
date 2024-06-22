bded04e82d8294bc48219ecd291f2158f8f92907
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-23 01:52:40 +0530

    Router Component

--- routers/src/ReactRouter.jsx
+++ routers/src/ReactRouter.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import NavBar from './Components/NavBar';
+import Error from './Components/Error';
 
 export default function ReactRouter() {
   return (
     <BrowserRouter>
+    <NavBar/>
     <Routes>
-        <Route path='/' element={<div>Home Page</div>} ></Route>
-        <Route path='/about' element={<div>About Us</div>} ></Route>
-        <Route path='/contact' element={<div>Contact</div>} ></Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/login' element={<div>Login</div>}/>
+        <Route path='*' element={<Error/>}/>
     </Routes>
     </BrowserRouter>
   )
